fc8cfc886dcf70e9ce531cf2f90fcaa603459ef3
feJustino <fwxjustino@gmail.com>
2024-01-02 18:55:17 -0300

    feat: componentizando

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,21 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero.model';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule,HeroDetailComponent],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.scss'
 })
-export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'wolverine'
+export class HeroesComponent implements OnInit{
+
+  constructor(private heroService: HeroService) {}
+
+  heroes: Hero[] = []
+  selectedHero?: Hero;
+  ngOnInit(): void {
+    this.getHeroes()
   }
 
-  heroes = HEROES
+  getHeroes(): void {
+    this.heroes= this.heroService.getHeroes()
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero
+  }
+
+  onKey(event: KeyboardEvent, heroId: number) {
+    let focusedHero = heroId
+    if (event.key === "ArrowUp") {
+      focusedHero = --heroId
+    }
+    if (event.key === "ArrowDown") {
+      focusedHero = ++heroId
+    }
+    if(event.key === "Enter"){
+      document.getElementById(focusedHero.toString())?.click()
+    }
+    document.getElementById((focusedHero).toString())?.focus()
+    console.log(event.key)
+  }
 }
